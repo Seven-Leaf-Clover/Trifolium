e22fae3a76830a440f6534f74f14fc802b6835ab
Clover <FoundClovers_777@protonmail.com>
2026-03-10 01:10:06 -0600

    A couple of enhancement gates

--- tweaks/plantain/jokers/03rare_jokers.lua
+++ tweaks/plantain/jokers/03rare_jokers.lua
@@ -9,6 +9,7 @@ SMODS.Joker:take_ownership('pl_calculator', {
 SMODS.Joker:take_ownership('pl_quarry', {
     rarity = 2,
     config = { extra = { xmult_mod = 0.4, xmult = 1 } },
+    enhancement_gate = "m_stone",
 },true)
 
 SMODS.Joker:take_ownership('pl_lasagna', {
