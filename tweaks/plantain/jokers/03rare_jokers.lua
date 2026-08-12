c1418ef40d4a3c58f868d82446d097c72d975d92
Clover <FoundClovers_777@protonmail.com>
2026-08-11 22:54:12 -0600

    New update match!

--- tweaks/plantain/jokers/03rare_jokers.lua
+++ tweaks/plantain/jokers/03rare_jokers.lua
@@ -6,12 +6,6 @@ SMODS.Joker:take_ownership('pl_calculator', {
     cost = 9,
 },true)
 
-SMODS.Joker:take_ownership('pl_quarry', {
-    rarity = 2,
-    config = { extra = { xmult_mod = 0.4, xmult = 1 } },
-    enhancement_gate = "m_stone",
-},true)
-
 SMODS.Joker:take_ownership('pl_lasagna', {
     cost = 9,
 },true)
