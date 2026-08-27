bb42baeb9fbfa484d94e5088168084012812eae5
Clover <FoundClovers_777@protonmail.com>
2026-08-26 23:46:23 -0600

    Adding some windows of appearance

--- tweaks/vanilla/vouchers/vouchers.lua
+++ tweaks/vanilla/vouchers/vouchers.lua
@@ -2,10 +2,34 @@ SMODS.Voucher:take_ownership('tarot_merchant', {
     in_pool = function()
         return (G.GAME.round_resets.ante >= 3)
     end,
-},false)
+})
 
 SMODS.Voucher:take_ownership('planet_merchant', {
     in_pool = function()
         return (G.GAME.round_resets.ante >= 3)
     end,
-},false)
+})
+
+SMODS.Voucher:take_ownership('telescope', {
+    in_pool = function()
+        return (G.GAME.round_resets.ante >= 3)
+    end,
+})
+
+SMODS.Voucher:take_ownership('seed_money', {
+    in_pool = function()
+        return (G.GAME.round_resets.ante >= 3)
+    end,
+})
+
+SMODS.Voucher:take_ownership('directors_cut', {
+    in_pool = function()
+        return (G.GAME.round_resets.ante >= 3)
+    end,
+})
+
+SMODS.Voucher:take_ownership('blank', {
+    in_pool = function()
+        return (G.GAME.round_resets.ante >= 2)
+    end,
+})
