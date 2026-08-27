1236fe8d96561db10946f4176e8db0c8e146d9b9
Clover <FoundClovers_777@protonmail.com>
2026-08-26 23:45:23 -0600

    Ban more interest related stuff

--- tweaks/vanilla/decks/green.lua
+++ tweaks/vanilla/decks/green.lua
@@ -1,6 +1,9 @@
 SMODS.Back:take_ownership("green", {
     apply = function(self)
+     -- Banned Vouchers
         G.GAME.banned_keys['v_seed_money'] = true
         G.GAME.banned_keys['v_money_tree'] = true
+     -- Banned Jokers   
+        G.GAME.banned_keys['j_to_the_moon'] = true
     end
 })
