db06df3a678af7dc7064bf5787ab824bb7acd1ec
Clover <FoundClovers_777@protonmail.com>
2026-08-28 16:22:33 -0600

    Medrano Unban!!!!

--- tweaks/cmykl/bans.lua
+++ tweaks/cmykl/bans.lua
@@ -1,15 +1,3 @@
-local banned_cmykl_jokers = {
- -- Legendaries
-    "medrano",
-}
-
-for i=1,#banned_cmykl_jokers do
-    SMODS.Joker:take_ownership('cmykl_'..banned_cmykl_jokers[i], {
-        no_collection = true,
-        in_pool = function() return false end
-    }, true)
-end
-
 local banned_cmykl_boosters = {
     "mega_spectral_pack",  
 }
